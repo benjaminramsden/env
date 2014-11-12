b13f4e28aba3b79c5e694a0dce1988a672781864
Chris Tozer <chris.tozer@metaswitch.com>
2014-11-12 11:18:20 +0000

    Updated instructions

--- README.md
+++ README.md
@@ -10,6 +10,7 @@
 ### On SSH to a new system, temporary environment:
 
 * Sets up PS1 and terminal colors
-* `curl https://bitbucket.org/Chris__T/env/raw/master/linux/onssh.sh | bash`
+* `source /dev/stdin  <<< "$(curl https://bitbucket.org/Chris__T/env/raw/master/linux/onssh.sh)"`
+* ...this is weird since we need to have the environment set _int the current shell_, which won't happen if we just pipe it to Bash
 
 ## Windows
--- linux/onssh.sh
+++ linux/onssh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # To run on an SSH connection, for temporary set up
-#   `curl https://bitbucket.org/Chris__T/env/raw/master/linux/onssh.sh | bash`
+#   ``source /dev/stdin  <<< "$(curl https://bitbucket.org/Chris__T/env/raw/master/linux/onssh.sh)"``
 
-export TERM=xterm-256color
-export PS1='\[\e[0;34m\][\A]\[\e[m\] \[\e[1;36m\]\h\[\e[m\] \[\e[1;34m\]\w\[\e[m\]\n\[\e[0;37m\]\$\[\e[m\] \[\e[00m\]'
+TERM=xterm-256color
+PS1='\[\e[0;34m\][\A]\[\e[m\] \[\e[1;36m\]\h\[\e[m\] \[\e[1;34m\]\w\[\e[m\]\n\[\e[0;37m\]\$\[\e[m\] \[\e[00m\]'
