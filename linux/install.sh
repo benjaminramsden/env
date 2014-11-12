5a488d29ed9d5d3c4e557a86186e3c29526c0a07
Chris Tozer <chris.tozer@metaswitch.com>
2014-11-12 11:38:54 +0000

    Updated install script (in test mode), remove unnecessary script

--- linux/install.sh
+++ linux/install.sh
@@ -1,2 +1,31 @@
 #/bin/bash
-sudo apt-get install vim
+if [ `command -v yum` ]
+then
+  installer=yum
+elif [ `command -v apt-get` ]
+then
+  installer=apt-get
+else
+  echo "No yum or apt-get; giving up"
+  #exit 1
+fi
+
+# Decide whether to sudo
+sudo=sudo
+if [ `whoami` = "root" ]
+then
+  sudo=
+fi
+
+function prepare {
+  echo $sudo $installer -y update
+  echo $sudo $installer -y upgrade
+}
+function install {
+  echo $sudo $installer -y install $1
+}
+
+prepare
+
+install vim
+install tmux
